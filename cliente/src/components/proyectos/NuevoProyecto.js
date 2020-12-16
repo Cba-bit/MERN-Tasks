990673a67ee82d07c744898786455ba7c2abb8dd
Ignacio <ignaciotagle26@gmail.com>
2020-12-15 23:54:08 -0300

    creando el listado de tareas, el provider de proyectos

--- cliente/src/components/proyectos/NuevoProyecto.js
+++ cliente/src/components/proyectos/NuevoProyecto.js
@@ -1,6 +1,11 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useContext, useState } from 'react';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const NuevoProyecto = () => {
+    // Obtener el state del formulario
+    const proyectosContext = useContext(proyectoContext);
+    const { formulario } = proyectosContext;
+
     const [proyecto, setProyecto] = useState({
         nombre: '',
     });
@@ -33,24 +38,26 @@ const NuevoProyecto = () => {
                 Nuevo Proyecto
             </button>
 
-            <form
-                className='formulario-nuevo-proyecto'
-                onSubmit={onSubmitProyecto}>
-                <input
-                    type='text'
-                    className='input-text'
-                    placeholder='Nombre Proyecto'
-                    name='nombre'
-                    onChange={onChangeProyecto}
-                    value={nombre}
-                />
+            {formulario ? (
+                <form
+                    className='formulario-nuevo-proyecto'
+                    onSubmit={onSubmitProyecto}>
+                    <input
+                        type='text'
+                        className='input-text'
+                        placeholder='Nombre Proyecto'
+                        name='nombre'
+                        onChange={onChangeProyecto}
+                        value={nombre}
+                    />
 
-                <input
-                    type='submit'
-                    className='btn btn-block btn-primario'
-                    value='Agregar Proyecto'
-                />
-            </form>
+                    <input
+                        type='submit'
+                        className='btn btn-block btn-primario'
+                        value='Agregar Proyecto'
+                    />
+                </form>
+            ) : null}
         </Fragment>
     );
 };
