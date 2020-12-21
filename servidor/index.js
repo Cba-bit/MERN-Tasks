83188dece46e86a80ac0ae91c7a5b6e1c3a63f27
Ignacio <ignaciotagle26@gmail.com>
2020-12-21 20:02:32 -0300

    Creando server express, configurando Mongo DB

--- servidor/index.js
+++ servidor/index.js
@@ -1,1 +1,16 @@
-console.log('Desde index.js');
+const express = require('express');
+const conectarDB = require('./config/db');
+
+// Crear el servidor
+const app = express();
+
+// conectar a la DB
+conectarDB();
+
+// Puerto de la App
+const PORT = process.env.PORT || 4000;
+
+// Arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor está funcionando en el puerto ${PORT}`);
+});
